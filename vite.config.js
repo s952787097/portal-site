17cde4516f7b6c2cd715114730a7a9f64d46947a
songhanru <952787097@qq.com>
2022-11-21 01:15:47 +0800

    修改根目录

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pm",
-  publicDir: "/pm",
+  base: "/portal-site",
+  publicDir: "/portal-site",
   plugins: [react()],
 });
